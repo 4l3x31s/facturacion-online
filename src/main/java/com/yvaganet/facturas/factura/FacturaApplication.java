b842ded721db7ba3d88b6638ea300d8c683698e7
Alexeis <panthon.six@gmail.com>
2018-05-29 11:25:05 -0400

    Primera subida

---
+++ src/main/java/com/yvaganet/facturas/factura/FacturaApplication.java
@@ -0,0 +1,12 @@
+package com.yvaganet.facturas.factura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FacturaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FacturaApplication.class, args);
+	}
+}
